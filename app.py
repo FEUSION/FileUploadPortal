688c0b4ceb8b55bf398f9b4391022770b3bf5232
MLRS-dev <project.2023.bu@gmail.com>
2023-04-03 16:38:00 +0530

    Added new requirements

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, request
 import pandas as pd
 import psycopg2
 import os
+import io
+import xlrd
+import openpyxl
 from sqlalchemy import create_engine
 
 def abspathgen(path:str):
@@ -10,19 +13,21 @@ def abspathgen(path:str):
 
 app = Flask(__name__, template_folder=abspathgen("templates"))
 
-@app.route('/')
+@app.route('/', methods = ['GET','POST'])
 def home():
+
+    if request.method == 'POST':
+        file = request.files['file']
+        file_name = file.filename
+        file_data = file.read()
+
+        # if not file_name.endswith('.xlsx') and not file_name.endswith('.xls'):
+        #     return render_template("index.html", status=input_id)
+
+        data = pd.read_excel(io.BytesIO(file_data), engine='xlrd')
+
+        print(data)
     return render_template("index.html")
 
-# @app.route('/index.html',methods= ['GET','POST'])
-# def file_upload():
-#     if request.method == 'POST':
-#         file = request.files['file']
-#         file_name = file.filename
-#         file_data = file.read()
-
-#         if not file_name.endswith('.xlsx') and not file_name.endswith('.xls'):
-#             return render_template("index.html", status = "The file you are uploaded is not a spreadsheet!")
-        
 if __name__ == "__main__":
     app.run(debug=True)
